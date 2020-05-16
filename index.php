9d3ba13ef4497ff7ba195ccf7afcc5f7739d1850
tommy winata <tommywinata42@gmail.com>
2020-05-17 00:29:14 +0700

    menambahkn paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>hello world</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae dolorem similique ab mollitia odit nemo rem consequuntur suscipit eius beatae soluta explicabo nesciunt expedita alias laborum, sunt quis animi repellat.</p>
 </body>
 </html>
